f479164cf1606302125cacbec096a7ae5a5b0cbf
kama casta <kamacasta983@gmail.com>
2021-10-04 18:01:48 -0700

    finished viewAllEmployees function logic

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const inquirer = require('inquirer');
 const db = require('./db/conncection');
 const consoleTable = require('console.table');
 const { prompt } = require('inquirer');
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
 function initalSetup() {
   console.log(`
     
-    ----------------
+    ================
     Employee Tracker
-    ----------------
+    ================
     `);
     // Initital Prompt begins!
 }
@@ -47,4 +47,65 @@ prompt([
     }
 });
 
+function viewAllEmployees() {
+    console.log(`
+    ====================
+         Employees
+    ====================
+    `)
+    db.query('SELECT employee.first_name, employee.last_name, role.title, department.name, role.salary, manager.first_name, manager.lastname AS "manager_lastname" FROM employee LEFT JOIN role ON employee.role_id = role_id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id;',
+    function (err, res) {
+        if(err) throw err;
+        console.consoleTable(res);
+        initalSetup();
+    }
+    );
+    // const sql = "" 
+}
 
+function viewAllRoles() {
+    console.log(`
+    ====================
+          Roles
+    ====================
+    `)
+
+    // const sql = ""
+    
+}
+
+function viewAllDepartments() {
+    console.log(`
+    ====================
+        Departments
+    ====================
+    `)
+    // const sql = ""
+}
+
+function addEmployees() {
+    console.log(`
+    ====================
+       Added Employee
+    ====================
+    `)
+    // const sql = ""
+}
+
+function addRole() {
+    console.log(`
+    ====================
+        Role Added
+    ====================
+    `)
+    // const sql = 
+}
+
+function addDepartment() {
+    console.log(`
+    ====================
+      Added Department
+    ====================
+    `)
+    // const sql = 
+}
